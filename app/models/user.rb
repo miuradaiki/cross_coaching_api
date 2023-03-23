f6da6f7e9e62f81c485e3c78d6597b73de837c19
Daiki <daiki.miura@prum.jp>
2023-03-23 22:38:44 +0900

    add: gem annotate and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  birthday   :date
+#  email      :string(255)
+#  first_name :string(255)
+#  last_name  :string(255)
+#  sex        :integer
+#  uid        :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   validates :uid, presence: true
 end
