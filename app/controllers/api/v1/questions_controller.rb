ff808ea6ca471ad61e6b6490a8f44e323d0d1e7c
Daiki <daiki.miura@prum.jp>
2023-04-06 23:11:46 +0900

    update: questions_controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,10 +2,13 @@ module Api
   module V1
     class QuestionsController < ApplicationController
       def index
+        questions = Question.all
+        render json: questions
       end
 
       def show
-        questions = Question.all
+        question_id = params[:id]
+        questions = Question.find(question_id)
         render json: questions
       end
     end
