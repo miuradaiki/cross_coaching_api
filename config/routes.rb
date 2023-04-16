3a4e82ea3010afd6d99a74a3abb0760dd6591c26
Daiki <daiki.miura@prum.jp>
2023-04-16 19:40:34 +0900

    add: shares controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       resources :feedbacks, only: [:edit, :create, :destroy]
       resources :answers
       resources :questions
-      resources :shares
+      resources :shares, only: [:index]
     end
   end
 end
