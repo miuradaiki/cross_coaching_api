809d80faed533055b6f99e51423038468dd80826
Daiki <daiki.miura@prum.jp>
2023-05-13 00:02:36 +0900

    fix: error handling

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -2,18 +2,32 @@ module Api
   module V1
     class VotesController < ApplicationController
       def create
-        # TODO:既にレコードがある場合のエラーハンドリング
         user = User.find_by(uid: params[:user_id])
-        Vote.create(feedback_id: params[:feedback_id], user_id: user.id, vote_type: params[:vote_type])
+        if user
+          vote = Vote.new(vote_params.merge(user_id: user.id))
+          if vote.save
+            render json: vote, status: :created
+          else
+            render json: { errors: vote.errors }, status: :unprocessable_entity
+          end
+        else
+          render json: { error: "User not found" }, status: :not_found
+        end
       end
 
       def voted_feedbacks
         user = User.find_by(uid: params[:user_id])
-        voted_feedback_ids = Vote.where(user_id: user.id).pluck(:feedback_id)
+        if user
+          voted_feedback_ids = Vote.where(user_id: user.id).pluck(:feedback_id)
+          render json: voted_feedback_ids, status: :ok
+        else
+          render json: { error: "User not found" }, status: :not_found
+        end
+      end
 
-        render json: voted_feedback_ids, status: :ok
+      def vote_params
+        params.permit(:feedback_id, :vote_type)
       end
     end
   end
 end
-
