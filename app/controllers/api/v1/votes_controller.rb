3e9092e017504c855c69422b41944d6c173ca35a
Daiki <daiki.miura@prum.jp>
2023-05-27 00:35:44 +0900

    fix: votes_controller

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -2,26 +2,26 @@ module Api
   module V1
     class VotesController < ApplicationController
       def create
-        user = User.find_by(uid: params[:user_id])
-        if user
-          vote = Vote.new(vote_params.merge(user_id: user.id))
+        load_uid
+        if @user
+          vote = Vote.new(vote_params.merge(user_id: @user.id))
           if vote.save
             render json: vote, status: :created
           else
             render json: { errors: vote.errors }, status: :unprocessable_entity
           end
         else
-          render json: { error: "User not found" }, status: :not_found
+          render json: { error: "ユーザーが見つかりませんでした。" }, status: :not_found
         end
       end
 
       def voted_feedbacks
-        user = User.find_by(uid: params[:user_id])
-        if user
-          voted_feedback_ids = Vote.where(user_id: user.id).pluck(:feedback_id)
+        load_uid
+        if @user
+          voted_feedback_ids = Vote.where(user_id: @user.id).pluck(:feedback_id)
           render json: voted_feedback_ids, status: :ok
         else
-          render json: { error: "User not found" }, status: :not_found
+          render json: { error: "ユーザーが見つかりませんでした。" }, status: :not_found
         end
       end
 
@@ -29,6 +29,10 @@ module Api
 
       def vote_params
         params.permit(:feedback_id, :vote_type)
+      end
+
+      def load_uid
+        @user = User.find_by(uid: @payload["user_id"])
       end
     end
   end
